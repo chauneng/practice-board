0b78c0291a803b9fd0b93c935c73715b1360d170
m4r-zero <50446006+chauneng@users.noreply.github.com>
2022-11-10 15:16:34 +0900

    [add] global env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppConfigModule } from '../config/app-config.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
